7074a3ac312729b52bb4c1239acdf6596750653b
Shawn L Mansfield <36788749+slmansfield@users.noreply.github.com>
2021-08-29 12:21:00 -0400

    Update picture.py
    added image log function to keep track of selected images

--- picture.py
+++ picture.py
@@ -4,6 +4,10 @@ import os
 import pyheif
 from PIL import Image
 
+# impor internal python modules
+import logger
+
+log = logger.createLogger()
 
 def random_image():
     # path = r"/home/slmansfield/my_twitterbot/images"
@@ -11,7 +15,17 @@ def random_image():
     random_filename = random.choice(
         [x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))]
     )
+    add_selected_image_to_log("image_log.txt", random_filename)
+    log.info("Selected {} file".format(random_filename))
     return random_filename
+
+
+def add_selected_image_to_log(log_file, image_filename):
+    f = open(log_file, "a")
+    f.write(image_filename + "\n")
+    f.close()
+    log.info("Updated the log file with the latest randomly selected image")
+    return
 
 
 def convert_heif_to_jpg(image_filename):
@@ -25,7 +39,6 @@ def convert_heif_to_jpg(image_filename):
         heif_file.stride,
     )
     image.save("images/temp.jpg", "JPEG")
-
 
 def getPicture():
     picture = random_image()
